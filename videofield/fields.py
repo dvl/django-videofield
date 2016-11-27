7546a0d3ec3921fa02ba7c12842b3fc7634e95ce
André Luiz <contato@xdvl.info>
2016-11-27 21:14:21 -0200

    Add validation to check if video file is valid

--- videofield/fields.py
+++ videofield/fields.py
@@ -1,6 +1,67 @@
+import io
+import tempfile
+import subprocess
+
+try:
+    from subprocess import DEVNULL
+except ImportError:
+    DEVNULL = open('/dev/null', 'w')
+
+from django import forms
 from django.core import checks
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+
+class VideoFormField(forms.FileField):
+    default_error_messages = {
+        'invalid_video': _(
+            'Upload a valid video. The file you uploaded was either not an '
+            'video or a corrupted video.'
+        )
+    }
+
+    def to_python(self, data):
+        f = super(VideoFormField, self).to_python(data)
+
+        if f is None:
+            return None
+
+        if hasattr(data, 'temporary_file_path'):
+            input_file = data.temporary_file_path()
+        else:
+            if hasattr(data, 'read'):
+                content = data.read()
+            else:
+                content = data['content']
+
+            fd, input_file = tempfile.mkstemp()
+
+            with io.open(fd, 'w') as f:  # this works on Python 2?
+                f.write(str(content))
+
+        # tries to open the video file with ffmpeg to check his integrity
+        exit_code = subprocess.call([
+            'ffmpeg',
+            '-v', 'quiet',     # don't print messages
+            '-i', input_file,  # input file
+            '-f', 'null',      # format is null because we don't really want to convert anything
+            '-'                # output to nowhere
+        ], stdout=DEVNULL, stderr=DEVNULL)
+
+        # TODO: if file is an image ffmpeg dont exit with error code
+        # we should use ffprobe and analyze the output too
+
+        if not exit_code == 0:
+            raise forms.ValidationError(
+                self.error_messages['invalid_video'],
+                code='invalid_video'
+            )
+
+        if hasattr(f, 'seek') and callable(f.seek):
+            f.seek(0)
+
+        return f
 
 
 class VideoField(models.FileField):
@@ -11,6 +72,7 @@ class VideoField(models.FileField):
         self.height_field = kwargs.pop('height_field', None)
         self.duration_field = kwargs.pop('duration_field', None)
         self.size_field = kwargs.pop('size_field', None)
+        self.thumbnail_field = kwargs.pop('thumbnail_field', None)
 
         super(VideoField, self).__init__(*args, **kwargs)
 
@@ -21,9 +83,9 @@ class VideoField(models.FileField):
         return errors
 
     def _check_ffmpeg_installed(self):
-        ffmpeg_is_avaliable = False
+        exit_code = subprocess.call(['which', 'ffmpeg'], stdout=DEVNULL)
 
-        if not ffmpeg_is_avaliable:
+        if not exit_code == 0:
             return [
                 checks.Error(
                     'Cannot use VideoField because ffmpeg is not installed.',
@@ -46,6 +108,8 @@ class VideoField(models.FileField):
             kwargs['duration_field'] = self.duration_field
         if self.size_field:
             kwargs['size_field'] = self.size_field
+        if self.thumbnail_field:
+            kwargs['thumbnail_field'] = self.thumbnail_field
 
         return name, path, args, kwargs
 
